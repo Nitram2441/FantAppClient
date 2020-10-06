07ada38c01886a077c7b593d00d0baa4e167321b
Nitram2441 <Nitram2441@users.noreply.github.com>
2020-10-06 12:35:47 +0200

    Initial commit, fresh android studio project ver 7.0 with login template

---
+++ app/src/main/java/com/example/fantapp/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package com.example.fantapp.data;
+
+import com.example.fantapp.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ log in credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
